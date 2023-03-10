291c0190027bef34b279bc46609734340cb64047
nyan-ko <dumbstupidcatboy@gmail.com>
2023-03-09 22:16:13 -0500

    Delete example files

--- bot.py
+++ bot.py
@@ -1,17 +1,19 @@
-class Bot:
+import discord
+from discord.ext import commands
 
-    list of users
-    list of commands
-    current game tree
+intents = discord.Intents().default()
+intents.members = True
+intents.message_content = True
 
-    def startup()
-        ...
+bot = commands.Bot(command_prefix='!', description="Cat game!!!", intents=intents)
 
-    def add new user()
-        ...
-    
-    def meow meow()
-     ...
-    
-    ...
-    
+@bot.command()
+async def meow(ctx):
+    await ctx.send("Meow!")
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+bot.run("<insert token here>")
