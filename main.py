c8c7061efcef9a688342228d6a8a8b9fab4ef5bb
nyan-ko <dumbstupidcatboy@gmail.com>
2023-04-28 00:46:23 -0400

    Update main file and remove token

--- main.py
+++ main.py
@@ -7,14 +7,23 @@ This file is copyright (c) 2023 by Edric Liu, Janet Fu, Nancy Hu, and Lily Meng.
 
 import bot
 from user import User
-from test_tree import p, r
 from deserializer import TreeDeserializer
 from biome_generator import BiomeGenerator
+import user_interaction
+import reward_generator
 
 ###############################################################################
 # Initilizing the bot as a CatsCradle object (find this class in the bot.py
 # file) and running the bot token.
 ###############################################################################
 
-b = bot.CatsCradle(User(), TreeDeserializer(p, r), BiomeGenerator())
-b.run('MTA4MzI1MDc2NzI1MjY5MzA1NQ.Gs5xjl.iye3xFQcgFtslDBo1gFeecT6j78A2Ws0azwIq8')
+if __name__ == "__main__":
+    files = ["data/dialogue/arid.csv", "data/dialogue/frigid.csv",
+        "data/dialogue/temperate.csv", "data/dialogue/tropical.csv", "data/dialogue/urban.csv"]
+    
+    p = user_interaction.load_dialogue_generator(files)
+    r = reward_generator.load_reward_generator("cats.csv")
+    d = TreeDeserializer(p, r)
+    
+    b = bot.CatsCradle(User(), TreeDeserializer(p, r), BiomeGenerator())
+    b.run('token')
